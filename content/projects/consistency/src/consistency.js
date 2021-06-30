c80276ac7fb2af2292c343297f8d9bd3bda67393
John Westhoff <johnjwesthoff@gmail.com>
2021-06-30 16:20:18 -0400

    make thing not awful

---
+++ content/projects/consistency/src/consistency.js
@@ -0,0 +1,42 @@
+import newline from "common.js";
+export class ConsistencyModel {
+    getmem(context, process, object) {
+        //
+    }
+
+    putmem(conext, process, writer, object, value) {
+        //
+    }
+
+    get(context, ast_node) {
+        var key = _eval(context, ast_node.children[1]).eval.value;
+        var result_node = this.getmem(context, context.machine, key)["val"];
+        newline("<u>get</u> " + key + " = " + result_node.eval.value, c);
+        return result_node;
+    }
+
+    wait(conext, ast_node) {
+        var key = _eval(conext, ast_node.children[1]).eval.value;
+        var val = _eval(conext, ast_node.children[2]).eval.value;
+        var back = 1;
+        if (n.children.length > 3) {
+            back = _eval(conext, ast_node.children[3]).eval.value;
+        }
+        var neval = (val == this.getmem(conext, conext.machine, key)["val"].value);
+        ast_node.eval = new Node("boolean", neval, 0);
+        conext.loops[ast_node.line] = (conext.loops[ast_node.line] || 0) + 1;
+        if (!neval && conext.loops[ast_node.line] < 100) {
+            conext.steps[conext.machine] -= back;
+        } else {
+            newline(neval ? "met criteria " + key + " = " + val : "early termination, " + key + " != " + val, c);
+        }
+    }
+
+    put(context, ast_node) {
+        //
+    }
+
+    run(context) {
+        //
+    }
+}
--- content/projects/consistency/src/seqcst.js
+++ content/projects/consistency/src/seqcst.js
@@ -1,14 +1,17 @@
-function seqcst(c) {
-    function getmem(c, p, x) {
+import newline from "common.js";
+import { ConsistencyModel } from "./consistency.js";
+export class SequentialConsistency extends ConsistencyModel {
+
+    getmem(c, p, x) {
         c.machines[p][x] = c.machines[p][x] || { "val": gUndefined };
         return c.machines[p][x];
     }
 
-    function write(p, x, v, clk) {
+    write(p, x, v, clk) {
         return { "index": x, "from": p, "val": v, "clk": clk };
     }
 
-    function putmem(c, p, k, x, v) {
+    putmem(c, p, k, x, v) {
         //in context c, for process p, put the value v into object x
         //with the write issued from k
         var val = getmem(c, p, x);
@@ -17,7 +20,7 @@ function seqcst(c) {
         return val;
     }
 
-    function stepqueues(c, x) {
+    stepqueues(c, x) {
         while (Math.random() > 0.5 && c.machines[x]["out"].length > 0) {
             var write = c.machines[x]["out"].shift();
             newline("sending " + write["index"], c);
@@ -33,7 +36,7 @@ function seqcst(c) {
             newline("applying " + write["index"] + " = " + write["val"].value, c);
         }
     }
-    function step(c, x) {
+    step(c, x) {
         stepqueues(c, x);
         if (c.steps[x] < gStuff[x].length) {
             _eval(c, gStuff[x][c.steps[x]]);
@@ -41,49 +44,48 @@ function seqcst(c) {
         stepqueues(c, x);
     }
 
-    gFunMap['put'] = function (c, n) {
+    put(c, n) {
         var key = _eval(c, n.children[1]).eval.value;
         var val = _eval(c, n.children[2]).eval;
-        var wrt = putmem(c, c.machine, c.machine, key, val);
+        this.putmem(c, c.machine, c.machine, key, val);
         c.machines[c.machine]["out"].push(write(c.machine, key, val, c.machines[c.machine]["clk"].slice()));
         n.eval = val;
         newline("<u>put</u> " + key + " = " + n.eval.value, c);
     }
 
-    #include "get.js"
-    #include "wait.js"
-
-    c.machines = [];
-    c.steps = [];
-    c.done = [];
-    c.loops = {};
-    gEmptyVector = [];
-    gSteps = 0;
-    for (var i = 0; i < gStuff.length; i++) {
-        c.steps.push(1);
-        gEmptyVector.push(0);
-        c.done.push(false);
-        c.machines.push({
-            "mem": {},
-            "out": [],
-            "in": []
-        });
-    }
-
-    for (var i = 0; i < gStuff.length; i++) {
-        c.machines[i]["clk"] = gEmptyVector.slice();
-    }
-
-    while (!c.done.every((x) => x)) {
-        gSteps++;
-        var x = Math.floor(Math.random() * gStuff.length);
-        if (!c.done[x]) {
-            c.done[x] = (c.steps[x] >= gStuff[x].length) && ((c.machines[x]["in"].length == 0 && c.machines[x]["out"].length == 0) || gSteps > 1000000);
+    run(c) {
+        c.machines = [];
+        c.steps = [];
+        c.done = [];
+        c.loops = {};
+        var gEmptyVector = [];
+        this.gSteps = 0;
+        for (var i = 0; i < gStuff.length; i++) {
+            c.steps.push(1);
+            gEmptyVector.push(0);
+            c.done.push(false);
+            c.machines.push({
+                "mem": {},
+                "out": [],
+                "in": []
+            });
         }
-        if (!c.done[x]) {
-            c.machine = x;
-            step(c, x);
-            c.steps[x] += 1;
+
+        for (var i = 0; i < gStuff.length; i++) {
+            c.machines[i]["clk"] = gEmptyVector.slice();
+        }
+
+        while (!c.done.every((x) => x)) {
+            this.gSteps++;
+            var x = Math.floor(Math.random() * gStuff.length);
+            if (!c.done[x]) {
+                c.done[x] = (c.steps[x] >= gStuff[x].length) && ((c.machines[x]["in"].length == 0 && c.machines[x]["out"].length == 0) || this.gSteps > 1000000);
+            }
+            if (!c.done[x]) {
+                c.machine = x;
+                step(c, x);
+                c.steps[x] += 1;
+            }
         }
     }
 
