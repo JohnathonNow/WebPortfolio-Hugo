1ecfc471d3219767edd76ce9b4dba9bc281fa362
John Westhoff <johnjwesthoff@gmail.com>
2024-04-10 08:11:54 -0700

    wow

--- static/hmgc/index.service.worker.js
+++ static/hmgc/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1712761684|3932850086";
+const CACHE_VERSION = "1712761856|4104984971";
 const CACHE_PREFIX = "HMGC-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
